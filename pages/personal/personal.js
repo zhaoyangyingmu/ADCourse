b19461ca06119a0cdc5d954579e397c11c61a0e1
DarkYoung <16302010059@fudan.edu.cn>
2019-06-07 20:34:20 +0800

    fix 使用npm管理第三方包

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,22 +1,72 @@
 // pages/personal/personal.js
+import rxwx, {
+    Rx
+} from 'rxjs-wx';
+
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        name: "Dark Young",
-        id: "无",
-        avatar: "../../static/images/avatar.jpg",
-        score: 10,
-        course: 3
+        name: 'Dark Young',
+        id: '无',
+        avatar: '../../static/images/avatar.jpg',
+        analysis: [{
+            value: '10',
+            unit: '分',
+            title: '累计学分'
+        }, {
+            value: '3',
+            unit: '门',
+            title: '参与课程'
+        }],
+        mine: [{
+            'icon': '/static/images/ic_task_completed.png',
+            'title': '我的笔记',
+            'target': '../notes/notes'
+        }, {
+            'icon': '/static/images/ic_like_selected.png',
+            'title': '我的收藏',
+            'target': '../collections/collections'
+        }]
+    },
+    getStudentInfo: function() {
+        var observable = Rx.Observable.create(function(observer) {
+            observer.next(1);
+            observer.next(2);
+            observer.next(3);
+            observer.complete();
+        });
+
+        console.log('before subscribe');
+        observable.observeOn(Rx.Scheduler.async) // 本来是同步的，变成了异步
+            .subscribe({
+                next: (value) => {
+                    console.log(value);
+                },
+                error: (err) => {
+                    console.log('Error: ' + err);
+                },
+                complete: () => {
+                    console.log('complete');
+                }
+            });
+        console.log('after subscribe');
+    },
+    navigateToTarget: function(event) {
+        let target = event.currentTarget.dataset.target;
+        console.log(target);
+        wx.navigateTo({
+            url: `../${target}/${target}`
+        })
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-
+        this.getStudentInfo();
     },
 
     /**
